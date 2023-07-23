91b790c247451333c2c842a6d0248ee94bf7982c
Morphisor <a.gallitri@gmail.com>
2023-07-23 11:53:50 +0200

    feat: level progression

--- src/map_builder/theme.rs
+++ src/map_builder/theme.rs
@@ -14,7 +14,8 @@ impl MapTheme for DungeonTheme {
     fn tile_to_render(&self, tile_type: TileType) -> FontCharType {
         match tile_type {
             TileType::Floor => to_cp437('.'),
-            TileType::Wall => to_cp437('#')
+            TileType::Wall => to_cp437('#'),
+            TileType::Exit => to_cp437('>')
         }
     }
 }
@@ -32,7 +33,8 @@ impl MapTheme for ForestTheme {
     fn tile_to_render(&self, tile_type: TileType) -> FontCharType {
         match tile_type {
             TileType::Floor => to_cp437(';'),
-            TileType::Wall => to_cp437('"')
+            TileType::Wall => to_cp437('"'),
+            TileType::Exit => to_cp437('>')
         }
     }
 }
